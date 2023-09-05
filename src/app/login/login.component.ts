8d4ae5f4f10e4dde64cf0a183166bc2a23b0afea
Joaquin <maidanajoquin10@gmail.com>
2023-09-04 21:05:34 -0300

    adregando la validacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+public miFormu!:FormGroup;
+
+constructor(private fb:FormBuilder) {
+
+}
+
+ngOnInit(): void {
+this.miFormu = this.createMyForm();
+}
+
+
+private createMyForm():FormGroup{
+  return this.fb.group({
+    usuario:['', [Validators.required]],
+    password:['',Validators.required]
+  });
+}
+
+public submitFormulario(){
+ if(this.miFormu.invalid){
+  return;
+ }
+
+
+ alert("Se enviara el formulario")
+ console.log(this.miFormu.value)
+}
+
+public get f():any{
+  return this.miFormu.controls;
+}
 
 }
